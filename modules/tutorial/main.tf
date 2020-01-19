cbd741b366f604ce0ff464b7d01479a45a629c2d
Lee Myring <mail@thinkstack.io>
2020-01-19 17:21:14 +0000

    feat(tutorial): add provisioner local-exec

--- modules/tutorial/main.tf
+++ modules/tutorial/main.tf
@@ -1,9 +1,18 @@
 resource "aws_instance" "example" {
   ami           = "ami-0bbc25e23a7640b9b"
   instance_type = "t2.micro"
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
 }
 
 resource "aws_eip" "ip" {
   vpc = true
   instance = aws_instance.example.id
+}
+
+resource "aws_instance" "another" {
+  ami           = "ami-0bbc25e23a7640b9b"
+  instance_type = "t2.micro"
 }
