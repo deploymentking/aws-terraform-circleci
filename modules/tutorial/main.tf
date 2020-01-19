dbac17267fcc8f7e6590292335e10d42cee62637
Lee Myring <mail@thinkstack.io>
2020-01-19 17:08:44 +0000

    feat(tutorial): add elastic ip

--- modules/tutorial/main.tf
+++ modules/tutorial/main.tf
@@ -2,3 +2,8 @@ resource "aws_instance" "example" {
   ami           = "ami-0bbc25e23a7640b9b"
   instance_type = "t2.micro"
 }
+
+resource "aws_eip" "ip" {
+  vpc = true
+  instance = aws_instance.example.id
+}
